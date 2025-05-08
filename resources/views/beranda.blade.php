6fafcf9224de7223387f6e1b13c5705f83e9c1ff
4mmang <ammangdeveloper@gmail.com>
2025-05-08 20:44:10 +0800

    [feat] navigation bar

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content') 
+<section style="margin-top: 7rem">
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+</section>
+@endsection
