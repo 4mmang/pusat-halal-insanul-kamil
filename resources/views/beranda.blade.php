7d80f2c6f33681fdc594d38a28ae238ba244a311
4mmang <ammangdeveloper@gmail.com>
2025-05-08 21:15:38 +0800

    [feat] slideshow

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.app')
-@section('content') 
-<section style="margin-top: 7rem">
-    <div class="container">
-        <div class="row">
+@section('content')
+    <section style="margin-top: 7rem">
+        <div class="container">
+            <div class="row">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://picsum.photos/id/1015/800/300" class="d-block w-100" alt="Slide 1">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/id/1016/800/300" class="d-block w-100" alt="Slide 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/id/1018/800/300" class="d-block w-100" alt="Slide 3">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
