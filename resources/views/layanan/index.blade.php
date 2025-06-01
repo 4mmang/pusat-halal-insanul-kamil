6a1e0c409b113bb9990a6fa8a03c34cb2f6045e4
4mmang <ammangdeveloper@gmail.com>
2025-06-01 13:24:25 +0800

    [feat] list layanan

--- resources/views/layanan/index.blade.php
+++ resources/views/layanan/index.blade.php
@@ -11,7 +11,7 @@
             </div>
         </div>
     </section>
-    <section class="py-5 mb-5">
+    <section class="py-5">
         <div class="container">
 
             <div class="row row-cols-1 row-cols-md-3 g-4 align-items-stretch">
@@ -21,7 +21,8 @@
                         <div class="card-body d-flex flex-column text-center text-default">
                             <h5 class="card-title">Usaha Besar/Tinggi</h5>
                             <h5 class="card-title">Rp12.500.000</h5>
-                            <p class="fst-italic mt-3">Belum termasuk biaya Audit/Pemeriksaan atas Bahan, Proses Produksi dan Produk</p>
+                            <p class="fst-italic mt-3">Belum termasuk biaya Audit/Pemeriksaan atas Bahan, Proses Produksi
+                                dan Produk</p>
                         </div>
                     </div>
                 </div>
@@ -31,7 +32,8 @@
                         <div class="card-body d-flex flex-column text-center text-default">
                             <h5 class="card-title">Usaha Menengah</h5>
                             <h5 class="card-title">Rp5.000.000</h5>
-                            <p class="fst-italic mt-3">Belum termasuk biaya Audit/Pemeriksaan atas Bahan, Proses Produksi dan Produk</p>
+                            <p class="fst-italic mt-3">Belum termasuk biaya Audit/Pemeriksaan atas Bahan, Proses Produksi
+                                dan Produk</p>
                         </div>
                     </div>
                 </div>
@@ -41,7 +43,8 @@
                         <div class="card-body d-flex flex-column text-center text-default">
                             <h5 class="card-title">Usaha Mikro / Kecil</h5>
                             <h5 class="card-title">Rp650.000</h5>
-                            <p class="fst-italic mt-3">Belum termasuk biaya Audit/Pemeriksaan atas Bahan, Proses Produksi dan Produk</p>
+                            <p class="fst-italic mt-3">Belum termasuk biaya Audit/Pemeriksaan atas Bahan, Proses Produksi
+                                dan Produk</p>
                         </div>
                     </div>
                 </div>
@@ -49,5 +52,45 @@
             </div>
         </div>
 
+    </section>
+
+    <section class="py-5 bg-body-secondary">
+        <div class="container">
+            <p class="fw-bold fs-2 text-default mb-5">Lingkup Usaha yang dilayani oleh LPH <span class="text-warning">Pusat Halal
+                    Insan Kamil</span></p>
+
+            <div class="row">
+                <div class="col-md-6 mb-4"> {{-- Kolom untuk "Barang" --}}
+                    <div class="card h-100 p-3"> {{-- Menggunakan border Bootstrap --}}
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold text-default">Barang</h5>
+                            <ul class="list-unstyled">
+                                <li>• Makanan dan Minuman</li>
+                                <li>• Kosmetik</li>
+                                <li>• Obat</li>
+                                <li>• Produk Kimiawi</li>
+                                <li>• Barang gunaan</li> 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-4"> {{-- Kolom untuk "Jasa" --}}
+                    <div class="card h-100 p-3">
+                        <div class="card-body">
+                            <h5 class="card-title fw-bold text-default">Jasa</h5>
+                            <ul class="list-unstyled">
+                                <li>• Penyembelihan</li>
+                                <li>• Pengolahan</li>
+                                <li>• Penyimpanan</li>
+                                <li>• Pengemasan</li>
+                                <li>• Pendistribusian</li>
+                                <li>• Penjualan</li>
+                                <li>• Penyajian</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
