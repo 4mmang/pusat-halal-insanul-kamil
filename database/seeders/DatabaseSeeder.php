27eceaaed013f53752281e17f657dbc2a64e8494
4mmang <ammangdeveloper@gmail.com>
2025-06-25 12:10:00 +0800

    [feat] avatar user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,40 +16,48 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::create([
-            'name' => 'arman',
-            'email' => 'arman@example.com',
-            'password' => Hash::make('1234')
-        ]);
+        // User::create([
+        //     'name' => 'arman',
+        //     'email' => 'arman@example.com',
+        //     'password' => Hash::make('1234')
+        // ]);
 
         User::create([
             'name' => 'dianfitrisari3395',
             'email' => 'dianfitrisari3395@gmail.com',
-            'password' => Hash::make('@Dfs3395')
+            'password' => Hash::make('@Dfs3395'),
+            'avatar' => '1.png'
         ]);
 
         User::create([
             'name' => 'Harfiani1992',
             'email' => 'Harfiani1992@gmail.com',
-            'password' => Hash::make('insankamil25')
+            'password' => Hash::make('insankamil25'),
+            'avatar' => '2.png'
+
         ]);
 
         User::create([
             'name' => 'anandamagfira22',
             'email' => 'anandamagfira22@gmail.com',
-            'password' => Hash::make('insankamil25')
+            'password' => Hash::make('insankamil25'),
+            'avatar' => '5.png'
+
         ]);
 
         User::create([
             'name' => 'haeranaabdulllah',
             'email' => 'haeranaabdulllah@gmail.com',
-            'password' => Hash::make('Insankamil84')
+            'password' => Hash::make('Insankamil84'),
+            'avatar' => '4.png'
+
         ]);
 
         User::create([
             'name' => 'naharuddinalie',
             'email' => 'naharuddinalie@gmail.com',
-            'password' => Hash::make('aksinyata16')
+            'password' => Hash::make('aksinyata16'),
+            'avatar' => '3.png'
         ]);
     }
 }
